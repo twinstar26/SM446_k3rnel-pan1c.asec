099b94094f60dd4e591dd03532161ec8dcd9a0b7
DivyPatel9881 <divy9881@gmail.com>
2020-01-16 16:35:30 +0530

    First yaml file

--- NLU/nlu-engine.py
+++ NLU/nlu-engine.py
@@ -1,6 +1,7 @@
 import json
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_EN
+import sys
 
 seed = 42
 engine = SnipsNLUEngine(config=CONFIG_EN, random_state=seed)
@@ -9,10 +10,13 @@ engine = SnipsNLUEngine(config=CONFIG_EN, random_state=seed)
 # with io.open("dataset.json") as f:
 #     dataset = json.load(f)
 
-with open("dataset.json",encoding='utf-16', errors='ignore') as f:
+with open("dataset/json/jira.json",encoding='utf-16', errors='ignore') as f:
      dataset = json.load(f, strict=False)
 
 engine.fit(dataset)
 
-parsing = engine.parse("Hey, lights on in the lounge !")
+parsing = engine.parse(sys.argv[1])
+intents = engine.get_intents(sys.argv[1])
+
 print(json.dumps(parsing, indent=2))
+print(json.dumps(intents, indent=2))
