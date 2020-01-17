3ce03d73e7ad9c39be874f3686a2e3a7d604e4f8
twinstar26 <adveer_b17@it.vjti.ac.in>
2020-01-17 16:49:45 +0530

    bitbucket inflation complete

--- NLU/dataset/yaml/dataset_expand.py
+++ NLU/dataset/yaml/dataset_expand.py
@@ -2,11 +2,23 @@ import sys
 
 from nltk.corpus import wordnet
 
-sen1 = "me public repositories of [userName](atharvaveer) workspace"
-sen2 = "me public repositories of [userName](atharvaveer)"
+sen1 = "me commits made in [projectName](google) repository in [userName](atharvaveer) workspace"
+sen2 = "me commits made in [projectName](google) repository in [userName](atharvaveer)"
 
 
 for syn in wordnet.synsets(sys.argv[1]):
+    for name in syn.lemma_names():
+        with open('result', 'a') as f:
+            f.write(name + ' ' + sen1 + '\n')
+            f.write(name + ' ' + sen2 + '\n')
+
+for syn in wordnet.synsets(sys.argv[2]):
+    for name in syn.lemma_names():
+        with open('result', 'a') as f:
+            f.write(name + ' ' + sen1 + '\n')
+            f.write(name + ' ' + sen2 + '\n')
+
+for syn in wordnet.synsets(sys.argv[3]):
     for name in syn.lemma_names():
         with open('result', 'a') as f:
             f.write(name + ' ' + sen1 + '\n')
