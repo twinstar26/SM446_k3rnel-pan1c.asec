c67d60b491f7f7fb38156b2d828b8aa61e1c0c42
yash0530 <yashjain0530@gmail.com>
2020-01-18 04:10:23 +0530

    End to End done

--- NLU/static/js/script2.js
+++ NLU/static/js/script2.js
@@ -1,25 +1,22 @@
 $(".query-form" ).submit(function(e) {
     e.preventDefault();
-    $(".add-queries").html($(".add-queries").html() + `
-        <div class="alert alert-dark" role="alert">
-            ${$("#query").val()}
-        </div>
-    `);
-    let str = $("#query").val();
-    $("#query").val("");
+    if ($("#query").val() != "") {
+        $(".add-queries").html($(".add-queries").html() + `
+            <div class="alert alert-dark" role="alert">
+                ${$("#query").val()}
+            </div>
+        `);
+        let str = $("#query").val();
+        $("#query").val("");
 
-    $.post("/chatbot/", {
-        user_query: str
-    }, function(data, status) {
-        if (status == "success") {
-            $("#results").html(data);
-        } else {
-            $("#results").html("Some Error Occured Please Try Again...."); 
-        }
-    });
+        $.post("/chatbot/", {
+            user_query: str
+        }, function(data, status) {
+            if (status == "success") {
+                $("#results").html(data);
+            } else {
+                $("#results").html("Some Error Occured Please Try Again...."); 
+            }
+        });
+    }
 });
-
-$("#stt").on("click", (e) => {
-    e.preventDefault();
-    
-})
