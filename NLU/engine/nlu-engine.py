00f7bb28f065ce248bd51a75ddeff116daccb694
twinstar26 <adveer_b17@it.vjti.ac.in>
2020-01-18 02:31:38 +0530

    stackoverflow inflated

--- NLU/engine/nlu-engine.py
+++ NLU/engine/nlu-engine.py
@@ -8,18 +8,18 @@ engine = SnipsNLUEngine(config=CONFIG_EN, random_state=seed)
 
 datasetName = 'dataset1.json'
 
-with open("/k3rnel-pan1c.asec/NLU/dataset/json/"+datasetName, encoding='utf-16', errors='ignore') as f:
-    dataset = json.load(f, strict=False)
-# with open("/home/twinstar/projects/k3rnel-pan1c.asec/NLU/dataset/json/"+datasetName, encoding='utf-8', errors='ignore') as f:
+# with open("/k3rnel-pan1c.asec/NLU/dataset/json/"+datasetName, encoding='utf-16', errors='ignore') as f:
 #     dataset = json.load(f, strict=False)
+with open("/home/twinstar/projects/k3rnel-pan1c.asec/NLU/dataset/json/"+datasetName, encoding='utf-8', errors='ignore') as f:
+    dataset = json.load(f, strict=False)
 
 print("TRAINING THE ENGINE...")
 
 engine.fit(dataset)
 
-shutil.rmtree("/k3rnel-pan1c.asec/NLU/trained_model")
-engine.persist("/k3rnel-pan1c.asec/NLU/trained_model")
-# shutil.rmtree("/home/twinstar/projects/k3rnel-pan1c.asec/NLU/trained_model")
-# engine.persist("/home/twinstar/projects/k3rnel-pan1c.asec/NLU/trained_model")
+# shutil.rmtree("/k3rnel-pan1c.asec/NLU/trained_model")
+# engine.persist("/k3rnel-pan1c.asec/NLU/trained_model")
+shutil.rmtree("/home/twinstar/projects/k3rnel-pan1c.asec/NLU/trained_model")
+engine.persist("/home/twinstar/projects/k3rnel-pan1c.asec/NLU/trained_model")
 
 print("ENGINE TRAINED.")
