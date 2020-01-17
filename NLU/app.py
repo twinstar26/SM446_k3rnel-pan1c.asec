e5b9c447e02d7e678eb520a45a5499afdf975004
DivyPatel9881 <divy9881@gmail.com>
2020-01-18 02:49:25 +0530

    API Interface moved

--- NLU/app.py
+++ NLU/app.py
@@ -9,12 +9,15 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/chatbot/", methods=["GET", "POST"])
 def index():
+    parsing = engine.parse("How are you?")
+    print(parsing['intent'])
     if request.method == "POST":
         print("behwbehwehwjvdewvewjvh")
         user_query = request.form["user_query"]
         parsing = engine.parse(user_query)
+        print(parsing.intent)
         json_response = json.dumps(parsing, indent=2)
         url = ""
         requests.post(url, json_response)
@@ -22,5 +25,7 @@ def index():
     else:
         return "<h1>CHATBOT RUNNING....</h1>"
 
+# def GiveHTMLFromUserQuery(json_response):
+#     if 
 if __name__=="__main__":
     app.run(debug=True)
